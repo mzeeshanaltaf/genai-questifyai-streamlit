ceddd7a40c164d3893d30a080b609e9f33fbf13b
mzeeshanaltaf <154883001+mzeeshanaltaf@users.noreply.github.com>
2024-05-21 18:35:30 +0500

    Added support of Chapters

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from PIL import Image
 from io import BytesIO
 from util import *
 
-topics = {
+agriculture_topics = {
     'Agriculture': 'topics/1_Agriculture.jpg',
     'Farming Methods/Organic Farming in Pakistan': 'topics/2_fm_ofp.jpg',
     'Smart Farms': 'topics/3_smart_farms.jpg',
     'Irrigation': 'topics/4_Irrigation.jpg',
     'Arable Farming': 'topics/5_ArableFarming.jpg',
     'Wheat': 'topics/6_wheat.jpg',
+}
+world_bank_topics = {
+    'Role of World Bank/IMF': 'topics/wb_imf_role.jpg',
+    'Role of Global Economic Systems': 'topics/global_economic_system.jpg',
+    'Importance of Exporting Finished Goods': 'topics/finished_goods.jpg',
 }
 
 # --- PAGE SETUP ---
@@ -25,11 +30,23 @@ st.write("Upload an image of a text paragraph, select number of questions to be 
 # ---- SETUP SIDEBAR ----
 st.sidebar.title("Configuration")
 api_key, file_uploader = configure_apikey_sidebar()
+st.subheader('Select the Chapter')
+chapter = st.selectbox("Select the Chapter", ('Agriculture', 'WorldBank/IMF'),
+                       label_visibility="collapsed", disabled=not file_uploader)
+
 st.subheader('Select the Topic')
-topic = st.selectbox("Select the Topic", topics.keys(),
-                     label_visibility="collapsed", disabled=not file_uploader)
-st.write(topic)
-uploaded_image = topics[topic]
+uploaded_image = ''
+if chapter == 'Agriculture':
+    keys = agriculture_topics.keys()
+    topic = st.selectbox("Select the Topic", keys,
+                         label_visibility="collapsed", disabled=not file_uploader)
+    uploaded_image = agriculture_topics[topic]
+elif chapter == 'WorldBank/IMF':
+    keys = world_bank_topics.keys()
+    topic = st.selectbox("Select the Topic", keys,
+                         label_visibility="collapsed", disabled=not file_uploader)
+    uploaded_image = world_bank_topics[topic]
+
 image = Image.open(uploaded_image)
 byte_arr = BytesIO()
 image.save(byte_arr, format='PNG')
@@ -48,7 +65,8 @@ input_prompt = f"""
 You are a proficient text reader from an image. 
 We will upload an image of a text. After extracting text from the image, generate {questions} questions of 
 difficulty level: {difficulty}. Besides questions, also generate an answer to those questions. Type answers after all
-the questions.
+the questions. You don't need to respond with original text of the image. Your response should include only quesitons and
+answers.
 
 If image is not consisted of text, respond that there is no text in the image or similar. 
 """
